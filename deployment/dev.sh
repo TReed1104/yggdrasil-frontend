af7bc8a532483bc228a8c3498f27a96942231227
Tom Reed <t.reed1104@gmail.com>
2020-11-26 13:38:20 +0000

    Template change - deployment scripts updated

--- deployment/dev.sh
+++ deployment/dev.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+  #!/bin/sh
 echo ---------------------------------------
 echo Deployment Start - Development
 echo ---------------------------------------
-echo Building and Deploying Vue App
+echo Building and Deploying Yggdrasil Frontend
 echo ---------------------------------------
-docker-compose -p vue-app-dev -f docker-compose.dev.yml up -d --build --remove-orphans
+docker-compose -p yggdrasil-frontend-dev -f docker-compose.dev.yml up -d --build --remove-orphans
 echo
 echo ---------------------------------------
 echo Conntainer Status:
 echo ---------------------------------------
-docker ps | grep 'vue-app'
+docker ps | grep 'yggdrasil-frontend'
--- deployment/prod.sh
+++ deployment/prod.sh
@@ -2,11 +2,11 @@
 echo ---------------------------------------
 echo Deployment Start - Production
 echo ---------------------------------------
-echo Building and Deploying Vue App
+echo Building and Deploying Yggdrasil Frontend
 echo ---------------------------------------
-docker-compose -p vue-app -f docker-compose.yml up -d --build --remove-orphans
+docker-compose -p yggdrasil-frontend -f docker-compose.yml up -d --build --remove-orphans
 echo
 echo ---------------------------------------
 echo Conntainer Status:
 echo ---------------------------------------
-docker ps | grep 'vue-app'
+docker ps | grep 'yggdrasil-frontend'
